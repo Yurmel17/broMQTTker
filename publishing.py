3c9fb66f780ced4a9b4a73abde5ecc3c6c8da355
Yuro <50340148+Yurmel17@users.noreply.github.com>
2022-01-27 18:31:19 -0500

    Update publishing.py

--- publishing.py
+++ publishing.py
@@ -6,11 +6,10 @@ from paho.mqtt import client as mqtt_client
 
 broker = 'broker.emqx.io'
 port = 1883
-topic = "python/mqtt"
+topic_1 = "Noticias/UIS"
+topic_2 = "Noticias/SIU"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
-username = 'emqx'
-password = 'public'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -20,7 +19,6 @@ def connect_mqtt():
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client

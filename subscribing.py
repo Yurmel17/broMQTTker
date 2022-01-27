54ce9d8e7bd904ed388b39da9f7c9cdf472c2e98
Yuro <50340148+Yurmel17@users.noreply.github.com>
2022-01-27 18:30:46 -0500

    Update subscribing.py

--- subscribing.py
+++ subscribing.py
@@ -5,12 +5,10 @@ from paho.mqtt import client as mqtt_client
 
 broker = 'broker.emqx.io'
 port = 1883
-topic = "python/mqtt"
+topic_1 = "Noticias/UIS"
+topic_2 = "Noticias/SIU"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
-username = 'emqx'
-password = 'public'
-
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -20,17 +18,25 @@ def connect_mqtt() -> mqtt_client:
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
 
 def subscribe(client: mqtt_client):
+    
+    topic = input("Hola! Adelante podrá obtener la noticia más actualizada de esta semana, escoja el/los topic de su interés: \n")
+    
+    if topic == 1:
+        client.subscribe(topic_1)
+    elif topic == 2:
+        client.subscribe(topic_2)
+    else:
+        print("Failed to find a topic")
+    
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
 
-    client.subscribe(topic)
     client.on_message = on_message
 
 
